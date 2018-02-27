898baf056119461e56eebc406e54798c2d6b01d1
Steve <steve.shortt@gmail.com>
2018-02-27 14:07:04 -0600

    Initial check-in: added RSA and Base64 encode/decode

---
+++ zephyr.Crypto/RsaHelpers.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Zephyr.Crypto
+{
+    public class RsaHelpers
+    {
+        #region rsa
+        public static void GenerateRsaKeys(string keyContainerName, string pubPrivFilePath, string pubOnlyFilePath)
+        {
+            CspParameters cspParams = new CspParameters
+            {
+                KeyContainerName = keyContainerName
+            };
+            GenerateRsaKeys( cspParams, pubPrivFilePath, pubOnlyFilePath );
+        }
+        public static void GenerateRsaKeys(CspParameters cspParams, string pubPrivFilePath, string pubOnlyFilePath)
+        {
+            RSACryptoServiceProvider rsaKey = new RSACryptoServiceProvider( cspParams );
+
+            if( !string.IsNullOrEmpty( pubPrivFilePath ) )
+            {
+                using( StreamWriter sw = new StreamWriter( pubPrivFilePath ) )
+                {
+                    sw.Write( rsaKey.ToXmlString( true ) );
+                }
+            }
+
+            if( !string.IsNullOrEmpty( pubOnlyFilePath ) )
+            {
+                using( StreamWriter sw = new StreamWriter( pubOnlyFilePath ) )
+                {
+                    sw.Write( rsaKey.ToXmlString( false ) );
+                }
+            }
+        }
+
+        public static RSACryptoServiceProvider LoadRsaKeys(string keyContainerName, string filePath)
+        {
+            CspParameters cspParams = new CspParameters
+            {
+                KeyContainerName = keyContainerName
+            };
+            return LoadRsaKeys( cspParams, filePath );
+        }
+        public static RSACryptoServiceProvider LoadRsaKeys(string keyContainerName, string filePath, CspProviderFlags flags)
+        {
+            CspParameters cspParams = new CspParameters
+            {
+                KeyContainerName = keyContainerName,
+                Flags = flags
+            };
+            return LoadRsaKeys( cspParams, filePath );
+        }
+        public static RSACryptoServiceProvider LoadRsaKeys(CspParameters cspParams, string filePath)
+        {
+            RSACryptoServiceProvider rsaKey = new RSACryptoServiceProvider( cspParams );
+            if( File.Exists( filePath ) )
+            {
+                using( StreamReader sr = new StreamReader( filePath ) )
+                {
+                    rsaKey.FromXmlString( sr.ReadToEnd() );
+                }
+                return rsaKey;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static string Encrypt(string keyContainerName, string filePath, string value)
+        {
+            return Encrypt( keyContainerName, filePath, CspProviderFlags.NoFlags, value );
+        }
+        public static string Encrypt(string keyContainerName, string filePath, CspProviderFlags flags, string value)
+        {
+            RSACryptoServiceProvider rsa = LoadRsaKeys( keyContainerName, filePath, flags );
+            return Encrypt( rsa, value );
+        }
+
+        public static string Encrypt(RSACryptoServiceProvider rsa, string value)
+        {
+            byte[] valueBytes = ASCIIEncoding.ASCII.GetBytes( value );
+            byte[] encrypted = rsa.Encrypt( valueBytes, false );
+            return Convert.ToBase64String( encrypted );
+        }
+
+        public static string Decrypt(string keyContainerName, string filePath, string value)
+        {
+            return Decrypt( keyContainerName, filePath, CspProviderFlags.NoFlags, value );
+        }
+        public static string Decrypt(string keyContainerName, string filePath, CspProviderFlags flags, string value)
+        {
+            RSACryptoServiceProvider rsa = LoadRsaKeys( keyContainerName, filePath, flags );
+            return Decrypt( rsa, value );
+        }
+
+        public static string Decrypt(RSACryptoServiceProvider rsa, string value)
+        {
+            byte[] valueBytes = Convert.FromBase64String( value );
+            byte[] decrypted = rsa.Decrypt( valueBytes, false );
+            return ASCIIEncoding.ASCII.GetString( decrypted );
+        }
+        #endregion
+    }
+}
---
+++ zephyr.Crypto/StringHelpers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+
+namespace Zephyr.Crypto
+{
+    public class StringHelpers
+    {
+        #region base64
+        public static string Base64Encode(string value)
+        {
+            byte[] valueBytes = ASCIIEncoding.ASCII.GetBytes( value );
+            return Convert.ToBase64String( valueBytes );
+        }
+
+        public static string Base64EncodeFromBytes(byte[] valueBytes)
+        {
+            return Convert.ToBase64String( valueBytes );
+        }
+
+        public static string Base64Decode(string value)
+        {
+            byte[] valueBytes = Convert.FromBase64String( value );
+            return ASCIIEncoding.ASCII.GetString( valueBytes );
+        }
+
+        public static byte[] Base64DecodeToBytes(string value)
+        {
+            return Convert.FromBase64String( value );
+        }
+
+        public static bool TryBase64Decode(string encodedValue, out string decodedValue)
+        {
+            try
+            {
+                byte[] valueBytes = Convert.FromBase64String( encodedValue );
+                decodedValue = ASCIIEncoding.ASCII.GetString( valueBytes );
+                return true;
+            }
+            catch
+            {
+                decodedValue = null;
+                return false;
+            }
+        }
+        #endregion
+    }
+}
