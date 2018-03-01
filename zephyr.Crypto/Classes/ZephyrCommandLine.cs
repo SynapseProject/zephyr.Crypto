cab76d6b0cde90e27052bb7ecd4a64dd56f9437f
Steve <steve.shortt@gmail.com>
2018-03-01 11:34:51 -0600

    added CommandLine class, plus arranged project files

---
+++ zephyr.Crypto/Classes/ZephyrCommandLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ZephyrCommandLine
+{
+    public void Main(string[] args)
+    {
+        foreach( string arg in args )
+            Console.WriteLine( arg );
+
+        Environment.Exit( -1 );
+    }
+}
