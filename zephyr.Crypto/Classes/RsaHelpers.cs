158d6fa26286d9ef183d35002f92c27c77b1135c
Steve <steve.shortt@gmail.com>
2018-12-10 11:17:48 -0600

    added manual setup of RSA for .netstandard support

--- zephyr.Crypto/Classes/RsaHelpers.cs
+++ zephyr.Crypto/Classes/RsaHelpers.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using System.Xml;
 
 namespace Zephyr.Crypto
 {
@@ -66,14 +67,16 @@ namespace Zephyr.Crypto
                 {
                     Uri uri = new Uri( filePath );
                     string uriContent = WebRequestClient.GetString( uri.ToString() );
-                    rsaKey.FromXmlString( uriContent );
+                    try { rsaKey.FromXmlString( uriContent ); }
+                    catch { rsaKey.FromXmlStringZephyr( uriContent ); }
                 }
                 catch
                 {
                     try
                     {
                         using( StreamReader sr = new StreamReader( filePath ) )
-                            rsaKey.FromXmlString( sr.ReadToEnd() );
+                            try { rsaKey.FromXmlString( sr.ReadToEnd() ); }
+                            catch { rsaKey.FromXmlStringZephyr( sr.ReadToEnd() ); }
                     }
                     catch( Exception innerEx )
                     {
@@ -199,6 +202,56 @@ namespace Zephyr.Crypto
                 return false;
             }
 
+        }
+    }
+
+    public static class RsaExtensions
+    {
+        public static void FromXmlStringZephyr(this RSA rsa, string xmlString)
+        {
+            RSAParameters parameters = new RSAParameters();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml( xmlString );
+
+            if( xmlDoc.DocumentElement.Name.Equals( "RSAKeyValue" ) )
+            {
+                foreach( XmlNode node in xmlDoc.DocumentElement.ChildNodes )
+                {
+                    switch( node.Name )
+                    {
+                        case "Modulus": parameters.Modulus = (string.IsNullOrEmpty( node.InnerText ) ? null : Convert.FromBase64String( node.InnerText )); break;
+                        case "Exponent": parameters.Exponent = (string.IsNullOrEmpty( node.InnerText ) ? null : Convert.FromBase64String( node.InnerText )); break;
+                        case "P": parameters.P = (string.IsNullOrEmpty( node.InnerText ) ? null : Convert.FromBase64String( node.InnerText )); break;
+                        case "Q": parameters.Q = (string.IsNullOrEmpty( node.InnerText ) ? null : Convert.FromBase64String( node.InnerText )); break;
+                        case "DP": parameters.DP = (string.IsNullOrEmpty( node.InnerText ) ? null : Convert.FromBase64String( node.InnerText )); break;
+                        case "DQ": parameters.DQ = (string.IsNullOrEmpty( node.InnerText ) ? null : Convert.FromBase64String( node.InnerText )); break;
+                        case "InverseQ": parameters.InverseQ = (string.IsNullOrEmpty( node.InnerText ) ? null : Convert.FromBase64String( node.InnerText )); break;
+                        case "D": parameters.D = (string.IsNullOrEmpty( node.InnerText ) ? null : Convert.FromBase64String( node.InnerText )); break;
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception( "Invalid XML RSA key." );
+            }
+
+            rsa.ImportParameters( parameters );
+        }
+
+        public static string ToXmlStringZephyr(this RSA rsa, bool includePrivateParameters)
+        {
+            RSAParameters parameters = rsa.ExportParameters( includePrivateParameters );
+
+            return string.Format( "<RSAKeyValue><Modulus>{0}</Modulus><Exponent>{1}</Exponent><P>{2}</P><Q>{3}</Q><DP>{4}</DP><DQ>{5}</DQ><InverseQ>{6}</InverseQ><D>{7}</D></RSAKeyValue>",
+                  parameters.Modulus != null ? Convert.ToBase64String( parameters.Modulus ) : null,
+                  parameters.Exponent != null ? Convert.ToBase64String( parameters.Exponent ) : null,
+                  parameters.P != null ? Convert.ToBase64String( parameters.P ) : null,
+                  parameters.Q != null ? Convert.ToBase64String( parameters.Q ) : null,
+                  parameters.DP != null ? Convert.ToBase64String( parameters.DP ) : null,
+                  parameters.DQ != null ? Convert.ToBase64String( parameters.DQ ) : null,
+                  parameters.InverseQ != null ? Convert.ToBase64String( parameters.InverseQ ) : null,
+                  parameters.D != null ? Convert.ToBase64String( parameters.D ) : null );
         }
     }
 }
